c2602d66ee7ca36e7fc2be0567f7a2109d69ae69
msyed74 <146086036+msyed74@users.noreply.github.com>
2025-03-06 16:20:47 +0530

    calenderscreen intials

--- app/navigation/AppNavigator.jsx
+++ app/navigation/AppNavigator.jsx
@@ -6,7 +6,8 @@ import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from '../screens/HomeScreen';
 import InboxScreen from '../screens/InboxScreen';
 import AlumniCellScreen from '../screens/AlumniCellScreen';
-import NotFoundScreen from '../screens/NotFoundScreen'; // Handles unmatched routes
+import NotFoundScreen from '../screens/NotFoundScreen'; 
+import CalenderScreen from '../screens/CalenderScreen';// Handles unmatched routes
 
 const Drawer = createDrawerNavigator();
 const Stack = createStackNavigator();
@@ -14,8 +15,9 @@ const Stack = createStackNavigator();
 // Drawer Navigator (Main Navigation)
 function DrawerNavigator() {
   return (
-    <Drawer.Navigator initialRouteName="Home" screenOptions={{ headerShown: false }}>
+    <Drawer.Navigator initialRouteName="Home" screenOptions={{ headerShown: true }}>
       <Drawer.Screen name="Home" component={HomeScreen} />
+      <Drawer.Screen name="Calender" component={CalenderScreen} />
       <Drawer.Screen name="Inbox" component={InboxScreen} />
       <Drawer.Screen name="Alumni Cell" component={AlumniCellScreen} />
     </Drawer.Navigator>
